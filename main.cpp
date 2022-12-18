4ec6b75e026d81168ebfc1071043e16d5973b292
Rajdeep Bandopadhyay <rajdeepbandopadhyay@gmail.com>
2022-12-17 23:38:00 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,9 +8,9 @@ void recursive_handler (std::unordered_map< std::string, bool >& dirdata,
                         std::string& level, std::vector<std::thread>& thrds) {
     if (folderpath.length() <2) return ;
 
-    level = level + " --";
+    level = level + " --"; // level increment per recursive depth
 
-    dirdata[folderpath] = true;
+    dirdata[folderpath] = true; // watchdog update
 
     for ( boost::filesystem::recursive_directory_iterator end, dir(folderpath); 
                                                                 dir != end; 
@@ -19,8 +19,9 @@ void recursive_handler (std::unordered_map< std::string, bool >& dirdata,
 
         if(is_directory(*dir) && !dirdata[dir->path().string()])  
         {
-            std:: cout << "Folder: " << level << " " << dir->path().string() << std::endl;
+            std:: cout << "Folder: " << level << " " << dir->path().string() << std::endl; // print foldername before file names
 
+            // spawn the thread per recursive depth as soon as you see a folder
             auto thrd = [&]{
                 recursive_handler(std::ref(dirdata), std::ref(filedata), std::ref(dir->path().string()), std::ref(level), thrds);
             };
@@ -28,29 +29,29 @@ void recursive_handler (std::unordered_map< std::string, bool >& dirdata,
             thrds.push_back(std::move(t1));
 
         }
-        if(is_regular_file(*dir))  
+        if(is_regular_file(*dir)) // print all same level recursive depth files
         {
             std::string temp = dir->path().string();
             if (filedata.find(temp) == filedata.end())
-                filedata[temp] = false;
+                filedata[temp] = false; // watchdog update
             if (filedata[temp] == false)
             {
                 std:: cout << "File:   " << level << " " << dir->path().string() << std::endl;
-                filedata[temp] = true;
+                filedata[temp] = true; // watchdog update
             }
         }
     }
 
-    level = level.substr(0, level.size()-2);
+    level = level.substr(0, level.size()-2); // level decrement per recursive depth outscope
 }
 
 int main(int argc, char** argv)
 {
-    std::unordered_map< std::string, bool > filedata, dirdata;
-    std::vector<std::thread> thrds;
-    std::string t = "";
+    std::unordered_map< std::string, bool > filedata, dirdata; // watchdogs to keep track
+    std::vector<std::thread> thrds; // threads vector to keep and join later, avoid bottleneck
+    std::string t = ""; // used as level indicator
 
-    while (true)
+    while (true) // to keep monitoring
     {
         recursive_handler (dirdata, filedata, argv[1], t, thrds);
         for (std::thread & th : thrds)
